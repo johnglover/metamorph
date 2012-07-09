d4e2755dd41485de218bb4009afbbd1f8e73f3e1
John Glover <j@johnglover.net>
2012-07-09 23:41:35 +0200

    [examples] Add print statements to time scale example

--- examples/time_scale.py
+++ examples/time_scale.py
@@ -11,7 +11,13 @@ input_path = sys.argv[1]
 output_path = sys.argv[2]
 
 audio, sampling_rate = metamorph.read_wav(input_path)
+
+print 'Time scaling by a factor of 2...'
+
 ts = metamorph.TimeScale()
 ts.scale_factor = 2.0
+
 output = ts.process(audio)
 wav.write(output_path, sampling_rate, np.array(output * 32768, dtype=np.int16))
+
+print 'Done. Saved to', output_path
