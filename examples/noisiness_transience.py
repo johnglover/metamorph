11f7d738e0cec224a25ad3ebc196b1599d1a795c
John Glover <j@johnglover.net>
2012-09-19 10:16:38 +0100

    [examples] Update noisiness_transience example so that scaling values are set via the command line.

--- examples/noisiness_transience.py
+++ examples/noisiness_transience.py
@@ -3,23 +3,25 @@ import numpy as np
 import scipy.io.wavfile as wav
 import metamorph
 
-if not len(sys.argv) == 3:
-    print 'Usage:', __file__, '<input wav file> <output wav file>'
+if not len(sys.argv) == 6:
+    print 'Usage:', __file__, '<input wav file>',
+    print '<harmonic scale> <residual scale> <transient scale>',
+    print '<output wav file>'
     sys.exit(1)
 
 input_path = sys.argv[1]
-output_path = sys.argv[2]
-
+output_path = sys.argv[5]
 audio, sampling_rate = metamorph.read_wav(input_path)
 
-print 'Synthesising just the harmonic component (no transient or residual)...'
+print 'Scaling the harmonic component by:', sys.argv[2]
+print 'Scaling the residual component by:', sys.argv[3]
+print 'Scaling the transient component(s) by:', sys.argv[4]
 
 fx = metamorph.FX()
-fx.harmonic_scale = 1
-fx.residual_scale = 0
-fx.transient_scale = 0
-
+fx.harmonic_scale = float(sys.argv[2])
+fx.residual_scale = float(sys.argv[3])
+fx.transient_scale = float(sys.argv[4])
 output = fx.process(audio)
-wav.write(output_path, sampling_rate, np.array(output * 32768, dtype=np.int16))
 
+wav.write(output_path, sampling_rate, np.array(output * 32768, dtype=np.int16))
 print 'Done. Saved to', output_path
