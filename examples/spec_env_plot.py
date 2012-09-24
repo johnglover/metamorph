9d90b52c60535baf15283b32996021f4a3a5b800
John Glover <j@johnglover.net>
2012-09-24 11:20:14 +0200

    [examples] Tidy up spec_env_plot.py

--- examples/spec_env_plot.py
+++ examples/spec_env_plot.py
@@ -15,32 +15,30 @@ frame_size = 512
 
 input_path = sys.argv[1]
 audio, sampling_rate = metamorph.read_wav(input_path)
-audio = audio[len(audio) / 2: (len(audio) / 2) + frame_size]
 
 print 'Plotting spectral envelope from 1 frame in the middle of the sample'
 
-e = metamorph.SpectralEnvelope(order, env_size)
 pd = simpl.LorisPeakDetection()
 pd.frame_size = frame_size
 pd.max_peaks = env_size
 frames = pd.find_peaks(audio)
 
-freqs = np.zeros(len(frames[0].peaks))
-mags = np.zeros(len(frames[0].peaks))
+frame = frames[len(frames) / 2]
+freqs = np.array([p.frequency for p in frame.peaks])
+amps = np.array([p.amplitude for p in frame.peaks])
 
-for i in range(len(freqs)):
-    freqs[i] = frames[0].peaks[i].frequency
-    mags[i] = frames[0].peaks[i].amplitude
+e = metamorph.SpectralEnvelope(order, env_size)
+env = e.envelope(freqs, amps)
 
-env = e.env(freqs, mags)
+if np.max(amps) > 0:
+    amps = 20 * sp.log10(amps)
 
-mags = 20 * sp.log10(mags)
-mags -= max(mags)
-env = 20 * sp.log10(env)
-env -= max(env)
+if np.max(env) > 0:
+    env = 20 * sp.log10(env)
+
 env_freqs = np.linspace(0, sampling_rate / 2, len(env))
 
-plt.plot(freqs, mags, 'r+')
+plt.plot(freqs, amps, 'r+')
 plt.plot(env_freqs, env, 'b-')
 plt.ylabel('Magnitude (dB)')
 plt.xlabel('Frequency (Hz)')
