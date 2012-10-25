c718cd2feed4fe418322e8d9c1f4e44313624a80
John Glover <j@johnglover.net>
2012-10-25 23:09:28 +0200

    [examples] Update time scale example to take the time scale factor as a command line argument.

--- examples/time_scale.py
+++ examples/time_scale.py
@@ -3,19 +3,21 @@ import numpy as np
 import scipy.io.wavfile as wav
 import metamorph
 
-if not len(sys.argv) == 3:
-    print 'Usage:', __file__, '<input wav file> <output wav file>'
+if not len(sys.argv) == 4:
+    print 'Usage:', __file__, '<input wav file> <time scale factor>',
+    print '<output wav file>'
     sys.exit(1)
 
 input_path = sys.argv[1]
-output_path = sys.argv[2]
+time_scale_factor = float(sys.argv[2])
+output_path = sys.argv[3]
 
 audio, sampling_rate = metamorph.read_wav(input_path)
 
-print 'Time scaling by a factor of 2...'
+print 'Time scaling by a factor of', time_scale_factor
 
 ts = metamorph.TimeScale()
-ts.scale_factor = 2.0
+ts.scale_factor = time_scale_factor
 
 output = ts.process(audio)
 wav.write(output_path, sampling_rate, np.array(output * 32768, dtype=np.int16))
