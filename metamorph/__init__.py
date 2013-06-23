941f7c61452bb5c4c35dc40dc1b432432f27e092
John Glover <j@johnglover.net>
2013-06-23 20:45:44 +0200

    Use scikits.audiolab to read wav files if available.
    scipy.io.wavfile doesn't read all wav file encodings.

--- metamorph/__init__.py
+++ metamorph/__init__.py
@@ -15,10 +15,16 @@ SpectralEnvelope = fx.SpectralEnvelope
 
 def read_wav(file):
     'return floating point values between -1 and 1'
-    sampling_rate, audio = wav.read(file)
+    try:
+        from scikits.audiolab import wavread
+        audio, sampling_rate, enc = wavread(file)
+        audio = np.asarray(audio, dtype=dtype)
+    except ImportError:
+        sampling_rate, audio = wav.read(file)
+        audio = np.asarray(audio, dtype=dtype) / 32768.0
 
     # if wav file has more than 1 channel, just take the first one
     if audio.ndim > 1:
         audio = audio.T[0]
 
-    return np.asarray(audio, dtype=dtype) / 32768.0, sampling_rate
+    return audio, sampling_rate
